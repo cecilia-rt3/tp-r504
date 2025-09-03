e68e78543a3c31b7b94f54586266ba21d4b822b7
boukaka <cecilia.boukaka@univ-rouen.fr>
2025-09-03 18:17:07 +0200

    saisie, puissance, gestion erreurs, tests OK

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,6 +1,9 @@
 # fonctions.py
-
 def puissance(a: int, b: int) -> int:
+    if not (type(a) is int and type(b) is int):
+        raise TypeError("Only integers are allowed")
     return a ** b
+
+
 
 
--- TP1/prog1.py
+++ TP1/prog1.py
@@ -1,1 +1,15 @@
 print("Hello, World!")
+
+# prog1.py
+import fonctions as f
+
+print("Boucle infinie — CTRL-C pour quitter.\n")
+
+while True:
+        a = int(input("Entrez un nombre (entier ou décimal) : "))
+        b = int(input("Entrez un nombre (entier ou décimal) : "))
+
+        res = f.puissance(a, b)
+        print(f"Carré de {a} = {res}\n")
+
+
