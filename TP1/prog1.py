9d7c00af251fd5d27b56f007a67ad3dbcf5f3ffe
boukaka <cecilia.boukaka@univ-rouen.fr>
2025-09-05 08:54:05 +0200

    MAJ de prog1.py

--- TP1/prog1.py
+++ TP1/prog1.py
@@ -3,14 +3,25 @@ print("Hello, World!")
 # prog1.py
 import fonctions as f
 
-print("Boucle infinie — CTRL-C pour quitter.\n")
+print("CTRL-C pour quitter")
 
 while True:
-        a = int(input("Entrez un nombre (entier ou décimal) : "))
-        b = int(input("Entrez un nombre (entier ou décimal) : "))
+      try:
+         a = int(input("Entrez un nombre a (entier ou décimal) : "))
+         b = int(input("Entrez un nombre b (entier ou décimal) : "))
 
-        res = f.puissance(a, b)
-        print(f"Carré de {a} = {res}\n")
+         res = f.puissance(a, b)
+         print(res)
+      
+      except ValueError:
+          print("Vuillez entrer des entiers")
+      except KeyboardInterrupt:
+          print("Au revoir")
+          break
 
+try:
+   f.puissance(2.0,3.0)
+except TypeError as e:
+    print ("Ok: ", e) 
 
 
