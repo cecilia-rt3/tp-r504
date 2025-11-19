52f927f541d62c3ef981c5f6a85108e627a00451
boukaka <cecilia.boukaka@univ-rouen.fr>
2025-11-19 11:32:25 +0100

    MAJ TP-LAB 19-11-2025

--- TP-LB/test_load.sh
+++
@@ -1,5 +0,0 @@
-for i in $(seq 1 500); do
-  echo -n "$i: "
-  curl -s http://localhost:83
-done
-
--- TP-LB/tp-C/appli/app.py
+++ TP-LB/tp-C/appli/app.py
@@ -1,10 +1,11 @@
 from flask import request, Flask
+import os
 import json
 
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-		return 'serveur 1'
+		return (os.environ.get('HOSTNAME') +"\n")
 
 if __name__ == '__main__':
 		app.run(debug=True, host='0.0.0.0')
